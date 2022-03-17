d0d4fbc3fc44ec67408610a3ed994de494fdca5e
vdaimon <rcdv13@gmail.com>
2022-03-17 19:13:01 +0300

    Refactored transaction model

--- FBA_UserAPI/Models/Transaction.cs
+++ FBA_UserAPI/Models/Transaction.cs
@@ -3,11 +3,10 @@
     public class Transaction
     {
         public int id { get; set; }
-        public User? User { get; set; }
         public DateTime? DateTime { get; set; }
         public decimal Amount { get; set; }
         public bool IsIncome { get; set; }
+        public int UserId { get; set; }
 
-        public Transaction() { }
     }
 }
